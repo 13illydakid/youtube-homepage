485d26bd1621e1f9e29acc6a88fe60c51b497aa5
Billy Chiu <billychiu517@gmail.com>
2023-10-16 06:12:29 -0700

    update seeders

--- src/data/sidebar.ts
+++ src/data/sidebar.ts
@@ -1,39 +1,39 @@
 export const subscriptions = [
   {
-    channelName: "",
-    id: "",
+    channelName: "J Perm",
+    id: "J Perm",
     imgUrl:
-      "",
+      "https://yt3.ggpht.com/ytc/APkrFKYkAgDYRbEnAO9-BlZnfDj2mmOdLT2boV28-FFUSg=s48-c-k-c0x00ffffff-no-rj",
   },
   {
-    channelName: "",
-    id: "",
+    channelName: "Grissini Project",
+    id: "Grissini Project",
     imgUrl:
-      "",
+      "https://yt3.ggpht.com/ytc/APkrFKaJIBd6dnqCJ5cQhnpsN44ASxUIHzwkCxaektVB=s48-c-k-c0x00ffffff-no-rj",
   },
   {
-    channelName: "",
-    id: "",
+    channelName: "Adult Swim",
+    id: "Adult Swim",
     imgUrl:
-      "",
+      "https://yt3.ggpht.com/KIq94K1ZMmnxu14wvGRBQpALqFRe8t7Li8ki-MMzN8tjUzyH07Cwi8IgT-eVUX7Q20pPcKrJwQ=s48-c-k-c0x00ffffff-no-rj",
   },
   {
-    channelName: "",
-    id: "",
+    channelName: "Cruncyroll",
+    id: "Cruncyroll",
     imgUrl:
-      "",
+      "https://w7.pngwing.com/pngs/38/745/png-transparent-crunchyroll-manga-anime-television-school-manga-thumbnail.png",
   },
   {
-    channelName: "",
-    id: "",
+    channelName: "TODAY",
+    id: "TODAY",
     imgUrl:
-      "",
+      "https://yt3.ggpht.com/ytc/APkrFKZRucSXlbwsWWP17nEPo0F39VgfHDQsxu-xM8ze4jo=s48-c-k-c0x00ffffff-no-rj",
   },
   {
-    channelName: "",
-    id: "",
+    channelName: "Breaking Bad & Better Call Saul",
+    id: "Breaking Bad & Better Call Saul",
     imgUrl:
-      "",
+      "https://i.ytimg.com/an_webp/DvkYRhu-TP0/mqdefault_6s.webp?du=3000&sqp=CIa4tKkG&rs=AOn4CLB4z_gHhIwU1oalefgAJkFRr2nbXQ",
   },
 ]
 
