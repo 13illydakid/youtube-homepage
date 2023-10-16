5abb22600cf197c6320850936e8597262a584afd
Billy Chiu <billychiu517@gmail.com>
2023-10-15 22:53:01 -0700

    ready

--- src/App.tsx
+++ src/App.tsx
@@ -34,3 +34,17 @@ export default function App() {
     </SidebarProvider>
   )
 }
+
+{/* <div className="max-h-screen flex flex-col">
+<div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+  <div className="overflow-x-hidden px-8 pb-4">
+    <div className="sticky top-0 bg-white z-10 pb-4">
+      <CategoryPills
+        categories={categories}
+        selectedCategory={selectedCategory}
+        onSelect={setSelectedCategory}
+      />
+    </div>
+  </div>
+</div>
+</div> */}
